820c48b5822f706237ddc0a8c9b898ee779da645
Peter Arato <it.arato@gmail.com>
2024-11-19 08:44:53 -0500

    Sidewinder algorithm.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env::args;
+
 use rand::prelude::*;
 
 #[derive(Debug, Default)]
@@ -23,6 +25,7 @@ struct Maze {
 }
 
 impl Maze {
+    #[allow(unused)]
     fn new_empty(width: usize, height: usize) -> Maze {
         let mut cells: Vec<Cell> = vec![];
         cells.resize_with(width * height, Cell::new_empty);
@@ -45,6 +48,7 @@ impl Maze {
         }
     }
 
+    #[allow(unused)]
     fn binary_tree_maze_creation(&mut self) {
         let mut rng = rand::thread_rng();
 
@@ -65,26 +69,65 @@ impl Maze {
         }
     }
 
+    #[allow(unused)]
+    fn sidewinder_maze_creation(&mut self) {
+        let mut rng = rand::thread_rng();
+        let mut run_length: usize;
+
+        for y in 0..self.height {
+            run_length = 0;
+
+            for x in 0..self.width {
+                let i = y * self.width + x;
+
+                if y == 0 && x == self.width - 1 {
+                    continue;
+                } else if x == self.width - 1 {
+                    // Check length of run.
+                    // Pick on randomly and erast north.
+                    let run_rand_i = rng.gen_range(0..=run_length);
+                    self.cells[i - run_rand_i].paths[0] = false;
+
+                    run_length = 0;
+                } else if y == 0 {
+                    self.cells[i].paths[1] = false;
+                } else {
+                    if rng.gen_range(0..=1) == 0 {
+                        // Check length of run.
+                        // Pick on randomly and erast north.
+                        let run_rand_i = rng.gen_range(0..=run_length);
+                        self.cells[i - run_rand_i].paths[0] = false;
+
+                        run_length = 0;
+                    } else {
+                        self.cells[i].paths[1] = false;
+                        run_length += 1;
+                    }
+                }
+            }
+        }
+    }
+
     fn dump(&self) {
         for y in 0..self.height {
-            print!("+");
+            print!("█");
             for x in 0..self.width {
                 let i = y * self.width + x;
 
                 if self.cells[i].paths[0] {
-                    print!("-+");
+                    print!("██");
                 } else {
-                    print!(" +");
+                    print!(" █");
                 }
             }
             print!("\n");
 
-            print!("|");
+            print!("█");
             for x in 0..self.width {
                 let i = y * self.width + x;
 
                 if self.cells[i].paths[1] {
-                    print!(" |");
+                    print!(" █");
                 } else {
                     print!("  ");
                 }
@@ -92,9 +135,9 @@ impl Maze {
             print!("\n")
         }
 
-        print!("+");
-        for x in 0..self.width {
-            print!("-+");
+        print!("█");
+        for _ in 0..self.width {
+            print!("██");
         }
 
         print!("\n\n");
@@ -102,7 +145,18 @@ impl Maze {
 }
 
 fn main() {
-    let mut maze = Maze::new_full(10, 6);
-    maze.binary_tree_maze_creation();
+    let args: Vec<String> = args().collect();
+
+    let width = args
+        .get(1)
+        .and_then(|raw_width| usize::from_str_radix(&raw_width, 10).ok())
+        .unwrap_or(10);
+    let height = args
+        .get(2)
+        .and_then(|raw_width| usize::from_str_radix(&raw_width, 10).ok())
+        .unwrap_or(10);
+
+    let mut maze = Maze::new_full(width, height);
+    maze.sidewinder_maze_creation();
     maze.dump();
 }
